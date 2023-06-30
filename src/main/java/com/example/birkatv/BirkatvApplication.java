239684ad762fcc92c86c9f71277b5b4b7caf8e6e
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2023-06-29 22:04:32 -0500

    primera subida

---
+++ src/main/java/com/example/birkatv/BirkatvApplication.java
@@ -0,0 +1,13 @@
+package com.example.birkatv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirkatvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirkatvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/birkatv/BirkatvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birkatv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirkatvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
